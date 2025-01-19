ece44d358c3dc98f33d8fe8c927f58cacbae316b
Sudip Thapa <js7096785@gmail.com>
2025-01-19 13:37:12 +0545

    Lil improve

---
+++ app/metadata.ts
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: 'Sudip Thapa - Design, Dev & Wizardry',
+  description: 'Sudip Thapa is a skilled web developer and designer specializing in creating immersive digital experiences. With expertise in React, Node.js, and modern web technologies, Sudip crafts innovative solutions that blend form and function.',
+  icons: {
+    icon: '/zoro2.ico',
+  },
+}; 
